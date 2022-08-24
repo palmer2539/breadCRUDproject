e0e2f39016c3b038924fdc81f06e6e547b08541b
palmer2539 <alexandr.palmer.dev@gmail.com>
2022-08-24 11:47:49 -0400

    updated titles

--- views/index.jsx
+++ views/index.jsx
@@ -2,9 +2,9 @@ const React = require('react');
 const { lastIndexOf } = require('../models/bread');
 const Default = require('./layouts/Default');
 
-function Index ({breads}) {
+function Index ({breads, title}) {
   return(
-    <Default>
+    <Default title={title}>
       <h2>Index Page</h2>
       {/* <p>I have {breads[0].name} bread!</p> */}
       <ul>
